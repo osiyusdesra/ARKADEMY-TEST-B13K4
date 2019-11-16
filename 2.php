d80191f92e04752e684a10d51ffdda1841e5e41e
osiyusdesra <57266553+osiyusdesra@users.noreply.github.com>
2019-11-16 17:43:12 +0700

    Memvalidasi username

---
+++ 2.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = "OSIYUSd";
+$pass = "111*ooo";
+   if (preg_match ('/^[A-Z]{7}$/', $username)) {
+       echo "Username benar";}
+       else{
+       echo "Username salah";
+   }
+   
+   
+
+?>
