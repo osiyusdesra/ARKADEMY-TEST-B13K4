ad38010219a52618f29d1ef3f2d602c17dcd1d0c
osiyusdesra <57266553+osiyusdesra@users.noreply.github.com>
2019-11-16 19:27:58 +0700

    Validasi Password

--- 2.php
+++ 2.php
@@ -7,7 +7,17 @@ $pass = "111*ooo";
        else{
        echo "Username salah";
    }
+
+echo "<br>";
+
+    $cr = '*';
+    $pw = preg_quote($cr, '/');
    
+ if (preg_match('/(^[0-9*]{3})(['.$pw.'])([[a-zA-Z*]{3,3}$)/', $pass)){
+       echo "Password benar";}
+       else{
+       echo "Password salah";
+   }
    
 
 ?>
